4a398eeeab5281128a69e821246e36b351ad55af
Geoff Wagstaff <geoff@gosquared.com>
2012-05-11 13:05:45 +0100

    Create properly

--- lib/deactivate_account.php
+++ lib/deactivate_account.php
@@ -1,15 +1,15 @@
 <?php
 
-class func_purge_account extends GS_ADMIN_SDK_request_model {
-	public $mode = 'purge_account';
+class func_deactivate_account extends GS_ADMIN_SDK_request_model {
+	public $mode = 'deactivate_account';
 	protected $params = array(
 		'user_id'
 	);
 	public function parse_response($data){
-		return new purge_account_response($data);
+		return new deactivate_account_response($data);
 	}
 }
 
-class purge_account_response extends GS_ADMIN_SDK_Response{
+class deactivate_account_response extends GS_ADMIN_SDK_Response{
 
 }
