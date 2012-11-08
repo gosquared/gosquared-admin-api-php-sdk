32a37eea0a481d2f5b51e336dddd5a83330a209c
Simon Tabor <me@simontabor.com>
2012-11-08 12:13:31 +0000

    send the referrer off too

--- lib/create_account.php
+++ lib/create_account.php
@@ -9,7 +9,8 @@ class func_create_account extends GS_ADMIN_SDK_request_model {
 	'first_name',
 	'last_name',
 	'referring_user',
-	'free_trial_end'
+	'free_trial_end',
+  'referrer'
     );
 
     public function parse_response($data) {
